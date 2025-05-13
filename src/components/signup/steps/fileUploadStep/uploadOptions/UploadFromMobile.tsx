332d25b23a7c8d3e5a14778385efab08a70d3a30
BertovDev <bertobautista18@gmail.com>
2025-05-13 03:20:59 -0300

    Fix lint

--- src/components/signup/steps/fileUploadStep/uploadOptions/UploadFromMobile.tsx
+++ src/components/signup/steps/fileUploadStep/uploadOptions/UploadFromMobile.tsx
@@ -5,7 +5,7 @@ import ConnectMobileComponent from "./ConnectMobileComponent";
 
 type Props = {
   handleUploadIdFile: (file: File) => void;
-  handleUploadImagePreview: (file: File, sessionId?: string) => void;
+  // handleUploadImagePreview: (file: File, sessionId?: string) => void;
   errors: {
     governmentIdFile?: {
       message?: string;
@@ -15,7 +15,7 @@ type Props = {
 
 export default function UploadFromMobile({
   handleUploadIdFile,
-  handleUploadImagePreview,
+  // handleUploadImagePreview,
   errors,
 }: Props) {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
