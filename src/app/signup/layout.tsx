c227d8445c63a9152597e65aa7cce54308738630
BertovDev <bertobautista18@gmail.com>
2025-05-14 00:39:42 -0300

    Remove commented code

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -26,12 +26,6 @@ export default function Layout({ children }: Props) {
         <div className="w-full max-w-5xl   flex">
           <Card className="w-full h-full flex flex-col items-center justify-center shadow-lg border-border ">
             {currentStep >= 0 && <StepsCounter isComplete={isComplete} />}
-            {/* <CardHeader className="pb-4">
-              <CardTitle className="text-2xl mb-5 font-bold text-center">
-                {isComplete && "Wallet Ready!"}
-              </CardTitle>
-            </CardHeader> */}
-
             <CardContent className="h-full flex  justify-start items-start   w-full">
               <motion.div
                 key={currentStep}
