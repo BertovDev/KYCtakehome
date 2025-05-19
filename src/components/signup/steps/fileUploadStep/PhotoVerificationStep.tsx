10dcfe70f05c86b61977b9a04a67cde27e952894
BertovDev <bertobautista18@gmail.com>
2025-05-19 00:35:54 -0300

    Add toast

--- src/components/signup/steps/fileUploadStep/PhotoVerificationStep.tsx
+++ src/components/signup/steps/fileUploadStep/PhotoVerificationStep.tsx
@@ -53,9 +53,22 @@ export default function PhotoVerificationStep({
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="space-y-5 flex flex-col justify-center gap-y-2 items-center pt-8 pb-5  file-upload"
+      className="space-y-5 flex flex-col justify-center gap-y-2 items-center pt-5 pb-5  file-upload"
     >
       <div className="flex  items-center  flex-col gap-y-1 ml-2 ">
+        <div className="text-sm flex flex-row items-center">
+          <motion.div
+            animate={{
+              opacity: [0, 1],
+              y: [-100, 0],
+              transition: { duration: 0.8 },
+            }}
+            className="flex flex-row items-center text-green-500"
+          >
+            ID photo successfully uploaded
+            <Check className="w-4 h-4 ml-2 " />
+          </motion.div>
+        </div>
         <h2 className="text-xl font-bold text-zinc-300">
           Profile Photo verification{" "}
         </h2>
